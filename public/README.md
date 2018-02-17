6c783ba4685d22139f2305fb21458cceb1edadaa
Jake Carns <jacob.carns@gmail.com>
2018-02-16 18:24:11 -0700

    initial commit

---
+++ public/README.md
@@ -0,0 +1,2 @@
+#Distributed Socket.io chat server
+An example of a distributed chat system using Socket.io and Redis.
---
+++ server.js
@@ -0,0 +1,81 @@
+const express         = require('express');
+const app             = express();
+const config          = require('./server.config');
+const bodyParser      = require('body-parser');
+const http            = require('http').Server(app);
+const io              = require('socket.io')(http);
+const uuid            = require('uuid/v4');
+const expSession      = require('express-session');
+const session         = expSession(config.session);
+const memStore        = require('memorystore')(expSession);
+const ioSession       = require('express-socket.io-session');
+const forceSSL        = require('./util/forceSSL');
+const port            = process.env.PORT || process.argv[2] || 3000;
+
+let store;
+
+app.use(bodyParser.json());
+
+if(process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', config.express.prod.trustProxy);
+  app.use(bodyParser.urlencoded(config.express.prod.bodyParser));
+  store = new memStore(config.express.prod.store);
+  app.use(forceSSL);
+} else {
+  app.set('trust proxy', config.express.dev.trustProxy);
+  app.use(bodyParser.urlencoded(config.express.dev.bodyParser));
+  store = new memStore(config.express.dev.store);
+}
+
+app.use(session);
+io.use(ioSession(session, { autoSave: true}));
+
+//Login Page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/', (req, res) => {
+  //TODO: handle login
+});
+
+//Register Page
+app.get('/register', (req, res) => {
+  if(req.session.user) {
+    res.redirect('/room/'+req.session.uuid);
+  } else {
+    res.sendFile(__dirname + '/public/register.html');
+  }
+});
+
+app.post('/register', (req, res) => {
+  //TODO: handle register
+});
+
+//Room Page
+app.get('/room*', (req, res) => {
+  if(req.session.user) {
+    res.sendFile(__dirname + 'public/room.html');
+  } else {
+    res.redirect(
+      [req.headers['x-forwarded-proto'], req.get('Host'), req.url].join('')
+    );
+  }
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+app.use(express.static('public'));
+
+//Handle sockets
+io.on('connection', (socket) => {
+  //TODO: handle sockets
+});
+
+//Start Server
+http.listen(port, () => {
+  console.log('listening on localhost:' + port);
+});
